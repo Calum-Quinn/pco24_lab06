cc21f7779cf5eb1b6d8cf0e086f2dca33ca48144
calum <calum@quinns.ch>
2024-12-11 19:29:40 +0100

    Update comments for more visibility

--- code/threadpool.h
+++ code/threadpool.h
@@ -13,9 +13,25 @@
 
 class Runnable {
 public:
+    /*
+    * An empy virtual destructor
+    */
     virtual ~Runnable() = default;
+
+    /*
+    * Function executing the Runnable task.
+    */
     virtual void run() = 0;
+
+    /*
+    * Function that can be called from the outside, to ask for the cancellation of the runnable.
+    * Shall be called by the threadpool if the Runnable is not started.
+    */
     virtual void cancelRun() = 0;
+
+    /*
+    * Simply retrieve an identifier for this runnable
+    */
     virtual std::string id() = 0;
 };
 
@@ -30,11 +46,13 @@ public:
     }
 
     /*
-     * Start a runnable. If a thread in the pool is avaible, assign the
-     * runnable to it. If no thread is available but the pool can grow, create a new
-     * pool thread and assign the runnable to it. If no thread is available and the
-     * pool is at max capacity and there are less than maxNbWaiting threads waiting,
-     * block the caller until a thread becomes available again, and else do not run the runnable.
+     * Start a runnable. 
+     * - If a thread in the pool is available, assign the runnable to it. 
+     * - If no thread is available but the pool can grow, create a new pool thread and assign the runnable to it. 
+     * - If no thread is available and the pool is at max capacity and there are less than maxNbWaiting threads waiting,
+     * block the caller until a thread becomes available again.
+     * - Else do not run the runnable.
+     * 
      * If the runnable has been started, returns true, and else (the last case), return false.
      */
     bool start(std::unique_ptr<Runnable> runnable) {
@@ -42,8 +60,8 @@ public:
         return false;
     }
 
-    /* Returns the number of currently running threads. They do not need to be executing a task,
-     * just to be alive.
+    /* Returns the number of currently running threads. 
+     * They do not need to be executing a task, just alive.
      */
     size_t currentNbThreads() {
         // TODO
